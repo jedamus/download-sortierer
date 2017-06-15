e68bbaeac310e4d66b03de4bedcfb2f44033ffc6
Leander Jedamus <ljedamus@web.de>
2017-06-16 01:58:14 +0200

    added os.path.join to construct paths

--- download-sortierer.py
+++ download-sortierer.py
@@ -2,7 +2,7 @@
 # coding=utf8
 
 # erzeugt Donnerstag, 08. Juni 2017 19:05 (C) 2017 von Leander Jedamus
-# modifiziert Freitag, 16. Juni 2017 01:43 von Leander Jedamus
+# modifiziert Freitag, 16. Juni 2017 01:57 von Leander Jedamus
 # modifiziert Montag, 12. Juni 2017 18:47 von Leander Jedamus
 # modifiziert Samstag, 10. Juni 2017 12:07 von Leander Jedamus
 # modifiziert Freitag, 09. Juni 2017 20:49 von Leander Jedamus
@@ -16,7 +16,7 @@ import re
 import gettext
 
 home = os.environ["HOME"];
-path_to_watch = home + "/" + "Downloads";
+path_to_watch = os.path.join(home,"Downloads");
 
 dict_suffix_and_path = {
   "dmg":       "dmg",
@@ -24,7 +24,7 @@ dict_suffix_and_path = {
   "iso":       "iso",
   "zip":       "zip",
   "deb":       "deb",
-  "pdf":       home + "/" + "Documents" + "/" + "pdf" + "/" + "download PDFs",
+  "pdf":       os.path.join(home,"Documents","pdf","download PDFs"),
   "tar.gz":    "tgz",
   "tar.xz":    "tgz",
   "tar.bzip2": "tgz",
@@ -49,8 +49,8 @@ for key in dict_suffix_and_path:
   suffix = re.sub("[.]","[.]","." + key);
   path = dict_suffix_and_path[key];
   if path[0] != "/":
-    path = path_to_watch + "/" + path;
-  regex = path_to_watch + "/" + ".*" + suffix;
+    path = os.path.join(path_to_watch,path);
+  regex = os.path.join(path_to_watch,".*" + suffix);
   compiled_key = re.compile(regex, re.UNICODE);
   dict_compiled_regex_and_path.update({ compiled_key: [key, suffix, path] });
 
@@ -66,29 +66,30 @@ class EventHandler(pyinotify.ProcessEvent):
           suffix_regex = dict_compiled_regex_and_path[key][1];
           suffix = dict_compiled_regex_and_path[key][0];
 
-          print(new_path);
-          print(suffix_regex);
-          print(suffix);
+          #print(new_path);
+          #print(suffix_regex);
+          #print(suffix);
           if not os.access(new_path, os.F_OK | os.X_OK):
             os.makedirs(new_path);
-          filename = re.sub(".*/(.*" + suffix_regex + ")","\g<1>",pathname);
+          filename = re.sub(os.path.join(".*","(.*") + suffix_regex + ")",
+            "\g<1>",pathname);
           filename_without_suffix = re.sub("(.*)" + suffix_regex,"\g<1>",
                                            filename);
-          print(filename);
-          print(filename_without_suffix);
+          #print(filename);
+          #print(filename_without_suffix);
 
-          new_filename = new_path + "/" + filename;
+          new_filename = os.path.join(new_path,filename);
           if os.access(new_filename, os.F_OK):
             for i in range(2,99):
               new_filename = filename_without_suffix + "({i:02d})".format(i=i) \
                 + "." + suffix;
-              if not os.access(new_path + "/" + new_filename, os.F_OK):
+              if not os.access(os.path.join(new_path,new_filename), os.F_OK):
                 break;
           n = pynotify.Notification(_("Download-Sorter"),
                 _("Moved {filename:s} from {frompath:s} to {topath:s} as {newfile:s}").format(filename=filename, frompath=path_to_watch, topath=new_path, newfile=new_filename));
           if not n.show():
             print(_("Failed to send notification"));
-          os.rename(pathname, new_path + "/" + new_filename);
+          os.rename(pathname, os.path.join(new_path,new_filename));
           break;
 
 handler = EventHandler()
