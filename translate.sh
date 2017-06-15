0965675b72a647812c94210b438d33a25a84d3e8
Leander Jedamus <ljedamus@web.de>
2017-06-16 01:51:00 +0200

    added gettext support for English and German

---
+++ translate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# erzeugt Freitag, 16. Juni 2017 01:46 (C) 2017 von Leander Jedamus
+# modifiziert Freitag, 16. Juni 2017 01:46 von Leander Jedamus
+
+translate.pl --file=download-sortierer.py --project=download-sortierer.py
+
+# vim:ai sw=2
+
