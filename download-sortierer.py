dcfcb3c1d33dd1ea876e909558e4cb476a2c0d99
Leander Jedamus <ljedamus@web.de>
2017-06-16 01:09:42 +0200

    added rename strategy for existing files

--- download-sortierer.py
+++ download-sortierer.py
@@ -2,7 +2,7 @@
 # coding=utf8
 
 # erzeugt Donnerstag, 08. Juni 2017 19:05 (C) 2017 von Leander Jedamus
-# modifiziert Freitag, 16. Juni 2017 00:51 von Leander Jedamus
+# modifiziert Freitag, 16. Juni 2017 01:06 von Leander Jedamus
 # modifiziert Montag, 12. Juni 2017 18:47 von Leander Jedamus
 # modifiziert Samstag, 10. Juni 2017 12:07 von Leander Jedamus
 # modifiziert Freitag, 09. Juni 2017 20:49 von Leander Jedamus
@@ -30,13 +30,13 @@ dict_suffix_and_path = {
 
 dict_compiled_regex_and_path = {};
 for key in dict_suffix_and_path:
-  suffix = re.sub("[.]","[.]",key);
+  suffix = re.sub("[.]","[.]","." + key);
   path = dict_suffix_and_path[key];
   if path[0] != "/":
     path = path_to_watch + "/" + path;
   regex = path_to_watch + "/" + ".*" + suffix;
   compiled_key = re.compile(regex, re.UNICODE);
-  dict_compiled_regex_and_path.update({ compiled_key: [suffix, path] });
+  dict_compiled_regex_and_path.update({ compiled_key: [key, suffix, path] });
 
 wm = pyinotify.WatchManager();  # Watch Manager
 mask = pyinotify.IN_CLOSE_WRITE # watched events
@@ -46,14 +46,29 @@ class EventHandler(pyinotify.ProcessEvent):
       pathname = event.pathname;
       for key in dict_compiled_regex_and_path:
         if key.match(pathname):
-          new_path = dict_compiled_regex_and_path[key][1];
+          new_path = dict_compiled_regex_and_path[key][2];
+          suffix_regex = dict_compiled_regex_and_path[key][1];
           suffix = dict_compiled_regex_and_path[key][0];
+
           print(new_path);
+          print(suffix_regex);
           print(suffix);
           if not os.access(new_path, os.F_OK | os.X_OK):
             os.makedirs(new_path);
-          filename = re.sub(".*/(.*[.].*)","\g<1>",pathname);
-          os.rename(pathname, new_path + "/" + filename);
+          filename = re.sub(".*/(.*" + suffix_regex + ")","\g<1>",pathname);
+          filename_without_suffix = re.sub("(.*)" + suffix_regex,"\g<1>",
+                                           filename);
+          print(filename);
+          print(filename_without_suffix);
+
+          new_filename = new_path + "/" + filename;
+          if os.access(new_filename, os.F_OK):
+            for i in range(2,99):
+              new_filename = new_path + "/" + filename_without_suffix + \
+                "({i:02d})".format(i=i) + "." + suffix;
+              if not os.access(new_filename, os.F_OK):
+                break;
+          os.rename(pathname, new_filename);
           break;
 
 handler = EventHandler()
