e42bbf3becfa7e586f2dc3320ced7092dd3f6b73
Leander Jedamus <ljedamus@web.de>
2020-12-14 14:24:40 +0100

    added install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# erzeugt Montag, 14. Dezember 2020 14:21 (C) 2020 von Leander Jedamus
+# modifiziert Montag, 14. Dezember 2020 14:22 von Leander Jedamus
+
+cp -Rvp download-sortierer.py locale $HOME/bin
+
+# vim:ai sw=2
+
